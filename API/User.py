58a96f1929e5ac446851e31bb65cd19fc8d87b0c
andygilet <andy-gilet@live.be>
2023-02-19 10:21:11 +0100

    oiijoj

--- API/User.py
+++ API/User.py
@@ -4,8 +4,8 @@ class User():
                  service_dog : bool, hard_hearing : bool,
                  visually_impaired : bool):
         self.name = name
-        self.level = xp / 1000
-        self.xp = xp % 1000,
+        self.level = int(xp / 1000)
+        self.xp = int(xp % 1000),
         self.wheelchair = wheelchair
         self.service_dog = service_dog
         self.hard_hearing = hard_hearing
